5649c039217f76592afd7794e8f863e773309f08
Oded Landau <odedlan8@gmail.com>
2022-09-14 21:27:59 +0300

    git setup between colaborators

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Airbnb is the best!!!
         </a>
       </header>
     </div>

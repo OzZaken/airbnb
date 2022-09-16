3cb228836bbfce67fd1e8bdd40d80d90dccd3e23
josh collins <joshcoll94@gmail.com>
2022-09-16 13:30:58 +0300

    renderd pics for preview

--- src/cmps/app-footer.jsx
+++ src/cmps/app-footer.jsx
@@ -1,5 +1,7 @@
 export function AppFooter() {
     return <section className="app-footer">
-        AppFooter
+        <p>© No rights reserved - this is a demo!</p>
+        <p>Privacy · Terms · Sitemap · Company Details</p>
     </section>
 }
+

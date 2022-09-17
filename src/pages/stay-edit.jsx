daea555a55719d6d3cf843cf7776e5c123c6ae2d
OzZaken <ozzaken0201@gmail.com>
2022-09-17 03:28:56 +0300

    StayEdit →  images upload to cloudinery on grid tamplate.

--- src/pages/stay-edit.jsx
+++ src/pages/stay-edit.jsx
@@ -1,7 +1,37 @@
+import { useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { ImgUploader } from "../cmps/img-uploader1"
+import { useForm } from "../hooks/useForm"
+import { Image } from 'cloudinary-react'
+
 export const StayEdit = () => {
+    const params = useParams()
+    const navigate = useNavigate()
+
+
     return (
         <section className="stay-edit">
             <h1>Stay Edit</h1>
+            <hr />
+            <div className="imgs-upload-container">
+                <div className="img-uploader item1">
+                <ImgUploader/>
+                </div>
+                <div className="img-uploader">
+                <ImgUploader/>
+                </div>
+                <div className="img-uploader">
+                <ImgUploader/>
+                </div>
+                <div className="img-uploader">
+                <ImgUploader/>
+                </div>
+                <div className="img-uploader">
+                <ImgUploader/>
+                </div>
+            </div>
         </section>
     )
 }
+
+

79750d7fa7a07f509eb6efe34ea56e2b7274b5c9
josh collins <joshcoll94@gmail.com>
2022-09-17 20:33:53 +0300

    try

--- src/cmps/stay-preview.jsx
+++ src/cmps/stay-preview.jsx
@@ -3,17 +3,6 @@ export function StayPreview({ stay, onRemoveStay }) {
   return (
     <li className="stay-preview">
       <Link to={`/stay/${stay._id}`} className="info">
-<<<<<<< HEAD
-        <img src={stay.imgUrls} alt="image" />
-        <div className="stay__info">
-          <h4>{stay.name}</h4>
-          <h4>{stay.type}</h4>
-          <h6>{stay.price}</h6>
-        </div>
-      </Link>
-      <Link to={`/stay/edit/${stay._id}`}>Edit</Link>
-    </div>
-=======
       <img src={stay.imgUrls[0]} alt="image"/>
       <div className="stay__info">
       <h4>{stay.name}</h4>
@@ -21,8 +10,7 @@ export function StayPreview({ stay, onRemoveStay }) {
         <h6>{stay.price}</h6>
       </div>
       </Link>
-        <Link to={`/stay/edit/${stay._id}`}>Edit</Link>
+        {/* <Link to={`/stay/edit/${stay._id}`}>Edit</Link> */}
     </li>
->>>>>>> 10022cd4551689158fd8ac9c5af5106fb50dfb7e
   )
 }

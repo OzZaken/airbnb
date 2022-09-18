5f9438305bdf157f6ca5d7137f3cacc71801afe8
Oded Landau <odedlan8@gmail.com>
2022-09-18 11:14:36 +0300

    img grid on stay-details

--- src/pages/stay-details.jsx
+++ src/pages/stay-details.jsx
@@ -3,10 +3,12 @@ import { useDispatch } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
 import { stayService } from '../services/stay.service.local'
 
+import StarRateIcon from '@mui/icons-material/StarRate';
+
 export const StayDetails = () => {
     const [stay, setStay] = useState(null)
     const params = useParams()
-    
+
     useEffect(() => {
         loadStay()
     }, [params.id])
@@ -23,21 +25,49 @@ export const StayDetails = () => {
     if (!stay) return <div>Loading...</div>
     return (
         <section className="stay-details">
-            <section>
-                <h3>Name: {stay.name}</h3>
-            </section>
-            <section>
-                <h3>Price: {stay.price}</h3>
-            </section>
-            <section>
-                <h3>
-                    Labels:
-                    <ul>
-                        {stay.labels &&
-                            stay.labels.map((label) => <li key={label}>{label}</li>)}
-                    </ul>
-                </h3>
-            </section>
+
+            <h1>{stay.name}</h1>
+
+            <div className="details flex align-center">
+
+                <div className="rate flex">
+                    <StarRateIcon />
+                    4.75
+                </div>
+
+                <p className="reviews">154 reviews</p>
+
+                <p className="address">{`${stay.loc.address},${stay.loc.country}`}</p>
+
+            </div>
+
+            <div className="imgs-grid-container">
+
+
+
+                <div>
+                    <img src={stay.imgUrls[0]} alt="Stay image" />
+                </div>
+                <div>
+                    <img src={stay.imgUrls[1]} alt="Stay image" />
+                </div>
+                <div>
+                    <img src={stay.imgUrls[2]} alt="Stay image" />
+                </div>
+                <div>
+                    <img src={stay.imgUrls[3]} alt="Stay image" />
+                </div>
+                <div>
+                    <img src={stay.imgUrls[4]} alt="Stay image" />
+                </div>
+            </div>
+
+            <h2>{`${stay.type} hosted by ${stay.host.fullname}`}</h2>
+
+            <h3>4 guests  1 bedroom  2 beds  1 bath </h3>
+
+
+
         </section>
     )
 }

f815c75af9bc20297f35bcada84bba554fd23708
OzZaken <ozzaken0201@gmail.com>
2022-09-18 14:07:02 +0300

    stayDetails imgs using map

--- src/pages/stay-details.jsx
+++ src/pages/stay-details.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
 import { stayService } from '../services/stay.service.local'
-
+import IosShareIcon from '@mui/icons-material/IosShare';
 import StarRateIcon from '@mui/icons-material/StarRate';
 
 export const StayDetails = () => {
@@ -13,10 +13,8 @@ export const StayDetails = () => {
         loadStay()
     }, [params.id])
 
-
     const loadStay = async () => {
         const stayId = params.stayId
-        console.log('params', params);
         const stay = await stayService.getById(stayId)
         setStay(stay)
     }
@@ -25,41 +23,19 @@ export const StayDetails = () => {
     if (!stay) return <div>Loading...</div>
     return (
         <section className="stay-details">
-
             <h1>{stay.name}</h1>
-
             <div className="details flex align-center">
-
                 <div className="rate flex">
-                    <StarRateIcon />
-                    4.75
+                    <StarRateIcon /> 4.75
                 </div>
-
-                <p className="reviews">154 reviews</p>
-
+                <p className="reviews">(154 reviews)</p>
                 <p className="address">{`${stay.loc.address},${stay.loc.country}`}</p>
-
             </div>
-
             <div className="imgs-grid-container">
 
-
-
-                <div>
-                    <img src={stay.imgUrls[0]} alt="Stay image" />
-                </div>
-                <div>
-                    <img src={stay.imgUrls[1]} alt="Stay image" />
-                </div>
-                <div>
-                    <img src={stay.imgUrls[2]} alt="Stay image" />
-                </div>
-                <div>
-                    <img src={stay.imgUrls[3]} alt="Stay image" />
-                </div>
-                <div>
-                    <img src={stay.imgUrls[4]} alt="Stay image" />
-                </div>
+                {stay.imgUrls.splice(5).map((imgUrl) => <div>
+                    <img src={imgUrl} alt="Stay image" />
+                </div>)}
             </div>
 
             <h2>{`${stay.type} hosted by ${stay.host.fullname}`}</h2>

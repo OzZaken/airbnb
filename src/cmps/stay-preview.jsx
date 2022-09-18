59907e5ad663999286d6a11d9399b7ae84d18240
josh collins <joshcoll94@gmail.com>
2022-09-18 20:47:23 +0300

    more scss

--- src/cmps/stay-preview.jsx
+++ src/cmps/stay-preview.jsx
@@ -5,13 +5,15 @@ export function StayPreview({ stay, onRemoveStay }) {
   return (
     <li className="stay-preview">
       <Link to={`/stay/${stay._id}`}>
-        <img src={stay.imgUrls[0]} alt="image" />
+        <div className="preview-img-container">
+          <img src={stay.imgUrls[0]} alt="image" />
+        </div>
         <div className="stay-info">
           <p className="stay-name">{`${stay.loc.city}, ${stay.loc.country}`}</p>
           <p className="stay-distance">1,109 kilometers</p>
           <p className="stay-date">Nov 30 - Dec 5</p>
         </div>
-        <p className="stay-price">{`${stay.price}$ night`}</p>
+        <p className="stay-price">{`$${stay.price} night`}</p>
       </Link>
       {/* <Link to={`/stay/edit/${stay._id}`}>Edit</Link> */}
     </li>

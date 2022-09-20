b2f161d93f930493b07213cde4f263390794def9
OzZaken <ozzaken0201@gmail.com>
2022-09-20 09:43:59 +0300

    temporery makeId on stay-achivments.jsx

--- src/cmps/stay-achievements.jsx
+++ src/cmps/stay-achievements.jsx
@@ -1,7 +1,7 @@
 import { stayService } from "../services/stay.service.local"
 import AppIcon from "./icon"
 
-import utilService from "../services/util.service"
+// import utilService from "../services/util.service"
 const achievements = stayService.getStayAchievements()
 
 export function StayAchievements({ stay }) {
@@ -9,7 +9,7 @@ export function StayAchievements({ stay }) {
     return <section className="flex column stay-achievements">
         {stay.achievements.splice(0, 3).map((achievement )=>
             <div 
-            // key={utilService.makeId()}
+            key={makeId()}
             className="flex">
                 <div>
                     <AppIcon iconKey={achievements[achievement].icon} />
@@ -22,4 +22,15 @@ export function StayAchievements({ stay }) {
         )}
 
     </section>
+}
+// todo: delete func
+function makeId(length = 6) {
+    var txt = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return txt;
 }

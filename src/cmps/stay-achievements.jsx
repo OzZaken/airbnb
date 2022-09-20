56f63f89fb23271969ad9030790b9f44ba1b444f
OzZaken <ozzaken0201@gmail.com>
2022-09-20 09:40:26 +0300

    on Bug → stay-achivments

--- src/cmps/stay-achievements.jsx
+++ src/cmps/stay-achievements.jsx
@@ -1,38 +1,25 @@
 import { stayService } from "../services/stay.service.local"
 import AppIcon from "./icon"
-const stayAchievements = stayService.getStayAchievements()
-export function StayAchievements({ stay }) {
 
+import utilService from "../services/util.service"
+const achievements = stayService.getStayAchievements()
+
+export function StayAchievements({ stay }) {
+    console.log('stay.achievements:', stay.achievements)
     return <section className="flex column stay-achievements">
-        {/* {stay.achievements.splice(0, 3).map(stayTitle =>
-    <div className="flex">
-        <div>{gStayAchievements[stay.achievements[0]].icon}</div>
-        <div>
-            <div>title</div>
-            <span>text title</span>
+        {stay.achievements.splice(0, 3).map((achievement )=>
+            <div 
+            // key={utilService.makeId()}
+            className="flex">
+                <div>
+                    <AppIcon iconKey={achievements[achievement].icon} />
+                </div>
+                <div>
+                    <div>{achievements[achievement].heading}</div>
+                    <span>{achievements[achievement].txt}</span>
+                </div>
             </div>
-    </div>
-)} */}
-        <div className="flex">
-            <div><AppIcon iconKey='wifi' /></div>
-            <div>
-                <h4>{stayAchievements[stay.achievements[0]].heading}</h4>
-                <span>{stayAchievements[stay.achievements[0]].txt}</span>
-            </div>
-        </div>
-        <div className="flex">
-            <div><AppIcon iconKey='wifi' /></div>
-            <div>
-                <h4>{stayAchievements[stay.achievements[1]].heading}</h4>
-                <span>{stayAchievements[stay.achievements[1]].txt}</span>
-            </div>
-        </div>
-        <div className="flex">
-            <div><AppIcon iconKey='wifi' /></div>
-            <div>
-                <h4>{stayAchievements[stay.achievements[2]].heading}</h4>
-                <span>{stayAchievements[stay.achievements[2]].txt}</span>
-            </div>
-        </div>
+        )}
+
     </section>
 }
